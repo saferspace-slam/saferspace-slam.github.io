2bd09604e387ebf7fb9661cd02de2f0d6c392e7b
max <mail@maxgietl.de>
2025-03-02 02:29:41 +0100

    deploy

--- _nuxt/C0-XWss4.js
+++
@@ -1,1 +0,0 @@
-const a=[{date:"2025-1-15"},{date:"2025-2-19"},{date:"2025-3-19"},{date:"2025-4-16"},{date:"2025-5-21"}],t={slams:a};export{t as default,a as slams};
---
+++ _nuxt/rhnZhA1-.js
@@ -0,0 +1,1 @@
+const a=[{date:"2025-01-15"},{date:"2025-02-19"},{date:"2025-03-19"},{date:"2025-04-16"},{date:"2025-05-21"}],t={slams:a};export{t as default,a as slams};
